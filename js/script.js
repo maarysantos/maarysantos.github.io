eaf5143aef59d7096b8ad1a1574d585fb8543b82
MaarySantos <maary.santos1@gmail.com>
2019-06-24 22:41:30 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,37 @@
+(function(){
+    var $menu = document.getElementsByName('header');
+
+    //menu movel
+    var $navigationMenu = document.querySelector('[data-js="btn_menu_nav"]');
+    $navigationMenu.addEventListener('click', navigation);
+    //window.addEventListener('click', navigation)
+
+    function navigation(){
+        document.body.classList.toggle('move')
+        document.body.classList.toggle('show');
+
+    };
+
+
+    window.addEventListener('scroll', setupNav);
+    
+    function setupNav(){
+        var posYScroll = getYscroll()
+        
+        if(posYScroll > 300 && !hasClassFx()){
+            document.body.classList.add('fx');
+        } 
+        
+        if(posYScroll <= 300 && hasClassFx()) {
+            document.body.classList.remove('fx');
+        }
+    }
+    
+    function getYscroll(){
+        return window.pageYOffset;
+    }
+    
+    function hasClassFx(){
+        return !!document.querySelector('.fx')
+    }
+})()
